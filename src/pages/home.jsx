80aaedbfe79cbd7e8ddb8fb8ea280ace16c8ee44
Isis Gonçalves Santos <isissantos8425@gmail.com>
2024-03-08 18:47:08 -0300

    adicionando pokemons na tela e botão com funcionalidade

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,57 +1,74 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-async function fetchPokemonKanto() {
+async function fetchPokemonKanto(offset) {
     try {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=151`);
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=151`);
         const pokemonData = response.data.results;
-        // console.log('deu certo', pokemonData);
+        
         return pokemonData.map(pokemon => pokemon.url);
     } catch (error) {
-        console.log('deu erro');
+        console.log('Erro ao buscar pokémons:', error);
+        return [];
     }
 }
 
 async function getPokemonData(pokeUrl) {
     try {
         const response = await axios.get(pokeUrl);
-
         const data = response.data;
-        // console.log(data.moves[0].move.name);
+        // console.log(data);
         return {
             id: data.id,
             name: data.name,
-            abilities : data.abilities[0].ability.name,
+            abilities: data.abilities.map(ability => ability.ability.name).join(", "),
             images: data.sprites.other.dream_world.front_default,
-            type: data.types[0].type.name
+            type: data.types.map(type => type.type.name).join(", "),
+            moves: data.moves.slice(0, 4).map(move => move.move.name).join(", ")
         };
-
     } catch (error) {
-        console.error('Erro:', error);
+        console.error('Erro ao obter dados do pokémon:', error);
         return null;
     }
 }
 
-async function getPokeInfo() {
-    try {
-        const pokemonUrls = await fetchPokemonKanto();
-        const pokemonData = await Promise.all(pokemonUrls.map(url => getPokemonData(url)));
-        console.log(pokemonData);
-    } catch {
-        console.log('erro ao buscar informações dos Pokémon.');
-    }
-}
-
 export const Pokemon = () => {
-    const [pokemons, setPokemons] = useState([])
+    const [pokemons, setPokemons] = useState([]);
+    const [offset, setOffset] = useState(0);
+    const [loadedPokemonIds, setLoadedPokemonIds] = useState(new Set()); 
+    const [totalPokemonsToShow, setTotalPokemonsToShow] = useState(10);
+
+    async function fetchMorePokemons() {
+        const pokemonUrls = await fetchPokemonKanto(offset);
+        const newPokemonsData = await Promise.all(pokemonUrls.map(url => getPokemonData(url)));
+        const filteredPokemons = newPokemonsData.filter(pokemon => !loadedPokemonIds.has(pokemon.id));
+
+        setPokemons(prevPokemons => [...prevPokemons, ...filteredPokemons]);
+        setOffset(prevOffset => prevOffset + 10);
+
+        if (pokemons.length + 10 >= 151) {
+            setTotalPokemonsToShow(151);
+        }
+    }
 
     useEffect(() => {
-        getPokeInfo();
-    }, [])
+        fetchMorePokemons();
+    }, []); 
 
-   return (
-    <>
-
-    </>
-   )
-}
+    return (
+        <>
+            <div>
+                {pokemons.slice(0, totalPokemonsToShow).map(pokemon => (
+                    <div key={pokemon.id}>
+                        <img src={pokemon.images} alt={pokemon.name} />
+                        <li>Name: {pokemon.name}</li>
+                        <li>Abilities: {pokemon.abilities}</li>
+                        <li>Type: {pokemon.type}</li>
+                        <li>Moves: {pokemon.moves}</li>
+                    </div>
+                ))}
+            </div>
+            <button onClick={() => setTotalPokemonsToShow(prevTotal => prevTotal + 10)}>Load More</button>
+        </>
+    );
+};
